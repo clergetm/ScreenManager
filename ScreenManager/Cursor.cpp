b7c9146d959dcb556538f3c62ec6f7c418c8aae9
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-21 13:52:38 +0100

    feat: get_coordinates #2

--- ScreenManager/Cursor.cpp
+++ ScreenManager/Cursor.cpp
@@ -2,6 +2,15 @@
 
 namespace ScreenManager {
 	namespace Cursor {
+		/// <summary>
+		/// Getter of coordinates.
+		/// </summary>
+		/// <returns>Get a tuple of coordinates (x, y).</returns>
+		coordinates get_coordinates() {
+			CONSOLE_SCREEN_BUFFER_INFO csbi;
+			GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+			return make_tuple(csbi.dwCursorPosition.X, csbi.dwCursorPosition.Y);
+		}
 
 	}
 }
