79df73232c3497ab787bbefb63ed0db9d6daddf1
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-21 13:52:38 +0100

    feat: get_coordinates #2

--- ScreenManager/Cursor.h
+++ ScreenManager/Cursor.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <tuple>
+#include <Windows.h>
 
+using namespace std;
 namespace ScreenManager {
 
 	/// <summary>
 	/// Namespace for cursor related methods.
 	/// </summary>
 	namespace Cursor {
+		typedef tuple<short, short> coordinates;
+		coordinates get_coordinates();		// Getter of coordinates.
 
 	}
 }
