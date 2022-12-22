9d4ea8d11a004150a6d1f64fd928f5b623a1dd5b
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-22 11:24:39 +0100

    feat: move up #2

--- ScreenManager/Cursor.cpp
+++ ScreenManager/Cursor.cpp
@@ -16,8 +16,8 @@ namespace ScreenManager {
 		/// Set the coordinates of the cursor with x and y.
 		/// </summary>
 		/// <param name="x">value on x axis.</param>
-		/// <param name="y">value on y axis</param>
-		/// <returns>The ANSI Sequence to set the coordinates</returns>
+		/// <param name="y">value on y axis.</param>
+		/// <returns>The ANSI Sequence to set the coordinates.</returns>
 		string set_coordinates(short x, short y) {
 			return "\033[" + to_string(x+1) + ";" + to_string(y+1) + "H";
 		}
@@ -25,12 +25,20 @@ namespace ScreenManager {
 		/// <summary>
 		/// Set the coordinates of the cursor with a tuple.
 		/// </summary>
-		/// <param name="c"> a tuple of coordinates (x, y)</param>
-		/// <returns>The ANSI Sequence to set the coordinates</returns>
+		/// <param name="c"> a tuple of coordinates (x, y).</param>
+		/// <returns>The ANSI Sequence to set the coordinates.</returns>
 		string set_coordinates(coordinates c) {
 			return "\033[" + to_string(get<0>(c)+1) + ";" + to_string(get<1>(c)+1) + "H";
 		}
 
+		/// <summary>
+		/// Move the cursor up by a number of lines.
+		/// </summary>
+		/// <param name="l">number of lines.</param>
+		/// <returns>The ANSI Sequence to move the cursor up.</returns>
+		string move_up(short l) {
+			return "\033[" + to_string(l) + "A";
+		}	
 
 	}
 }
