26345f7b592c952fd59296cb9022409c5e83b28d
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-22 11:03:36 +0100

    feat: set_coordinates #2

--- ScreenManager/Cursor.cpp
+++ ScreenManager/Cursor.cpp
@@ -12,5 +12,25 @@ namespace ScreenManager {
 			return make_tuple(csbi.dwCursorPosition.X, csbi.dwCursorPosition.Y);
 		}
 
+		/// <summary>
+		/// Set the coordinates of the cursor with x and y.
+		/// </summary>
+		/// <param name="x">value on x axis.</param>
+		/// <param name="y">value on y axis</param>
+		/// <returns>The ANSI Sequence to set the coordinates</returns>
+		string set_coordinates(short x, short y) {
+			return "\033[" + to_string(x+1) + ";" + to_string(y+1) + "H";
+		}
+
+		/// <summary>
+		/// Set the coordinates of the cursor with a tuple.
+		/// </summary>
+		/// <param name="c"> a tuple of coordinates (x, y)</param>
+		/// <returns>The ANSI Sequence to set the coordinates</returns>
+		string set_coordinates(coordinates c) {
+			return "\033[" + to_string(get<0>(c)+1) + ";" + to_string(get<1>(c)+1) + "H";
+		}
+
+
 	}
 }
