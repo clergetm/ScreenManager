444e2ca76042b2f6d44b6f111b195a09cf89fac8
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-22 11:42:16 +0100

    feat: print on given position #2

--- ScreenManager/Cursor.h
+++ ScreenManager/Cursor.h
@@ -18,6 +18,8 @@ namespace ScreenManager {
 		string move_down(short);				// Move the cursor down by a number of lines.
 		string move_forward(short);				// Move the cursor forward by a number of lines.
 		string move_backward(short);			// Move the cursor backward by a number of lines.
+		void cout_on_pos(short, short, string);	// Set the coordinates with x and y and write the given message.
+		void cout_on_pos(coordinates, string);	// Set the coordinates with tuple and write the given message.
 
 	}
 }
