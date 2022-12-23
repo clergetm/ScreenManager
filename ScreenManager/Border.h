091b3dda5aa7add6daf2966e80dc684d04b475ad
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-23 15:11:41 +0100

    feat: simple border enum #1

--- ScreenManager/Border.h
+++ ScreenManager/Border.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace ScreenManager {
 	
@@ -6,6 +6,23 @@ namespace ScreenManager {
 	/// Namespace for border related methods.
 	/// </summary>
 	namespace Border {
+
+		/// <summary>
+		/// Enumeration of ascii characters used to draw a simple border.
+		/// </summary>
+		enum class simple_b : char {
+			v_line = char(179),
+			h_line = char(196),
+			center_left = char(180),
+			center_right = char(195),
+			center_top = char(193),
+			center_bottom = char(194),
+			cross = char(197),
+			top_left_corner = char(218),
+			top_right_corner = char(191),
+			bottom_left_corner = char(192),
+			bottom_right_corner = char(217),
+		};
 
 	}
 }
