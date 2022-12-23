402bb70bd02acb4cabcf25c50435a03729d62007
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-23 18:02:01 +0100

    fix: coordinates and ESC code global

--- ScreenManager/Cursor.h
+++ ScreenManager/Cursor.h
@@ -1,16 +1,16 @@
 #pragma once
-#include <tuple>
 #include <string>
 #include <Windows.h>
+#include "ScreenManager.h"
 
 using namespace std;
+using namespace ScreenManager;
 namespace ScreenManager {
 
 	/// <summary>
 	/// Namespace for cursor related methods.
 	/// </summary>
 	namespace Cursor {
-		typedef tuple<short, short> coordinates;
 		coordinates get_coordinates();			// Getter of coordinates.
 		string set_coordinates(short, short);	// Setter of coordinates with x and y.
 		string set_coordinates(coordinates);	// Setter of coordinates with tuple.
