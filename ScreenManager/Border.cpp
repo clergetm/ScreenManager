36e41b4bf8dd0aed40133be9035cc1415c74928a
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-23 16:19:31 +0100

    feat: draw double border #1

--- ScreenManager/Border.cpp
+++ ScreenManager/Border.cpp
@@ -81,5 +81,43 @@ namespace ScreenManager {
 				cout << double_b::v_line << move_backward(1) << move_down(1);
 			}
 		}
+
+		/// <summary>
+		/// Draw a rectangle between two points with a double line.
+		/// </summary>
+		/// <param name="x">x of starting point.</param>
+		/// <param name="y">y of starting point.</param>
+		/// <param name="end_x">x of ending point.</param>
+		/// <param name="end_y">y of ending point.</param>
+		void draw_double_border(short x, short y, short end_x, short end_y) {
+			assert(x < end_y || y < end_y);
+
+			// Left vertical line.
+			cout << set_coordinates(x + 1, y);
+			draw_double_vertical_line(end_x - x - 1);
+
+			// Right vertical line.
+			cout << set_coordinates(x + 1, end_y - 1);
+			draw_double_vertical_line(end_x - x - 1);
+
+			// Top horizontal line.
+			cout << set_coordinates(x, y) << double_b::top_left_corner;
+			draw_double_horizontal_line(end_y - y - 2);
+			cout << double_b::top_right_corner;
+
+			// Bottom horizontal line.
+			cout << set_coordinates(end_x, y) << double_b::bottom_left_corner;
+			draw_double_horizontal_line(end_y - y - 2);
+			cout << double_b::bottom_right_corner;
+		}
+
+		/// <summary>
+		/// Draw a rectangle between two coordinates with a double line.
+		/// </summary>
+		/// <param name="start">Starting coordinates.</param>
+		/// <param name="end">Ending coordinates.</param>
+		void draw_double_border(coordinates start, coordinates end) {
+			draw_double_border(get<0>(start), get<1>(start), get<0>(end), get<1>(end));
+		}
 	}
 }
