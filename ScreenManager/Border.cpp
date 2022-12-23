39275ecb9aba41ce5c6757c42f2f5b9eb721892e
Mathys Clerget <mathysclerget+github@gmail.com>
2022-12-23 15:46:07 +0100

    feat: draw simple horinzontal line #1

--- ScreenManager/Border.cpp
+++ ScreenManager/Border.cpp
@@ -3,5 +3,14 @@
 namespace ScreenManager {
 	namespace Border {
 
+		/// <summary>
+		/// Draw a horizontal line.
+		/// </summary>
+		/// <param name="l"> Length of the line.</param>
+		void draw_simple_horizontal_line(short l) {
+			for (short i = 0; i < l; i++) {
+				cout << simple_b::h_line;
+			}
+		}
 	}
 }
